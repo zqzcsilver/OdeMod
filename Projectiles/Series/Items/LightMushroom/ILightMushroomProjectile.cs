2188a53e777c7058f3b85316a042dbfda5906e0e
幽白银 <3122247915@qq.com>
2022-07-10 20:52:32 +0800

    改了一下弹幕框架

---
+++ Projectiles/Series/Items/LightMushroom/ILightMushroomProjectile.cs
@@ -0,0 +1,17 @@
+﻿using OdeMod.Projectiles.Series.Items;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeMod.Projectiles.Series.LightMushroom
+{
+    /// <summary>
+    /// 光蘑菇系列弹幕请继承此接口，属于<see cref="IOdeItemProjectile"/>
+    /// </summary>
+    internal interface ILightMushroomProjectile : IOdeItemProjectile
+    {
+    }
+}
