0d860f4b80083a79ec8ba21263c57d90e930b28f
幽白银 <3122247915@qq.com>
2022-07-11 12:35:34 +0800

    扩展NPC的框架

---
+++ NPCs/Enemies/IEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeMod.NPCs.Enemies
+{
+    /// <summary>
+    /// 表示小怪的生物接口，小怪请继承此接口。属于<see cref="IOdeNPC"/>
+    /// </summary>
+    internal interface IEnemy : IOdeNPC
+    {
+    }
+}
