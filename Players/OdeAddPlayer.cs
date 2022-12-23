d8eb52c1494a1f6d131353a3ca7ae7a001c62506
YongYao <2805335603@qq.com>
2022-12-23 19:20:07 +0800

    ？？？ 我的临时数据呢 我的暂存呢

--- Players/OdeAddPlayer.cs
+++ Players/OdeAddPlayer.cs
@@ -9,10 +9,28 @@ using Terraria.ID;
 namespace OdeMod.Players
 {
     /// <summary>
-    /// 只是测试用，之后会合并入ODEPlayer中去。
+    /// 只是测试用，防止出现之前的意外，之后会合并入ODEPlayer中去。
     /// </summary>
-    internal class OdeFoodPlayer : ModPlayer, IOdePlayer
+    internal class OdeAddPlayer : ModPlayer, IOdePlayer
     {
+        //诡灵系列
+        //麻了 抛弃中式恐怖，那玩意一个是现实中都不敢整那么多活，别玩个游戏给自己整不吉利了（x
+        //另外在开启时选择jumpscare的出现程度，虽说不怎么吓人（
+        //我为什么每次都要在晚上写这些东西 捏马 人物理上已经麻了
+        /// <summary>
+        /// 诡灵模式的开关
+        /// </summary>
+        public bool SpookyMode = false;
+        /// <summary>
+        /// 彩蛋系列 黑山羊幼崽
+        /// </summary>
+        public bool DarkYoung = false;
+        /// <summary>
+        /// 诡灵的等级，随着游戏的进展会提升诡异程度，触发更高等级/更多诡异的事件。
+        /// </summary>
+        public int SpookyLevel = 0;
+
+        //食物系列
         /// <summary>
         /// 饱腹
         /// </summary>
@@ -36,14 +54,18 @@ namespace OdeMod.Players
             for (int i = 0; i < Foodtype.Length; i++)
                 tag.Add("Food_type"+i, Foodtype[i]);
             tag.Add("FoodPutrefaction", FoodPutrefaction);
+            tag.Add("SpookyMode", SpookyMode);
         }
         public override void LoadData(TagCompound tag)
         {
+            //食物
             Satiety = tag.Get<int>("Satiety");
             FoodVariety = tag.Get<int>("FoodVariety");
             for (int i = 0; i < Foodtype.Length; i++)
                 Foodtype[i] = tag.Get<string>("Food_type" + i);
             FoodPutrefaction = tag.Get<int>("FoodPutrefaction");
+            //诡灵
+            SpookyMode = tag.Get<bool>("SpookyMode");
         }
         public bool Food_Variety_Test(Item item)
         {
@@ -81,6 +103,30 @@ namespace OdeMod.Players
                 Player.statDefense /= 2;
             }
             base.UpdateBadLifeRegen();
+        }
+        //实验区（测试区 之后会不断改）
+        /// <summary>
+        /// 触发一些灵异事件
+        /// </summary>
+        /// <param name="level">灵异事件的等级</param>
+        /// <param name="strangeStyle">指定具体的灵异事件</param>
+        public void StrangeEvent(int level, int strangeStyle)
+        {
+            switch (strangeStyle)
+            {
+                case 0:
+                    Main.NewText("Error");
+                    break;
+                case 1:
+                    Main.NewText("Test");
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void StrangeEvent(int level)
+        {
+
         }
     }
 }
